9d0ab43e3226bc85801dd78ea20e014d294e0e7d
nortetech-admin <admin@nortetech.com.mx>
2025-06-16 21:30:23 -0600

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercer_repo',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     author='Filiberto Flores',
     author_email='floresfiliberto52@gmail.com',
