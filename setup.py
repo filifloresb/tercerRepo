5a2fa93c32f7d50701d765ced7130977a4f2f636
nortetech-admin <admin@nortetech.com.mx>
2025-06-16 23:03:32 -0600

    lanzamiento 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tercer_repo',
+    name='tercerRepo',
     version='0.1.0',
     packages=find_packages(),
     author='Filiberto Flores',
