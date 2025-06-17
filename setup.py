cb7b755902f5e0647f9aa4f95c3c75e9c7d41a8b
nortetech-admin <admin@nortetech.com.mx>
2025-06-16 21:06:41 -0600

    lanzamiento

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tercer_repo',
+    version='0.1',
+    packages=find_packages(),
+    author='Filiberto Flores',
+    author_email='floresfiliberto52@gmail.com',
+    url='https://github.com/filifloresb/tercerRepo#',
+)
